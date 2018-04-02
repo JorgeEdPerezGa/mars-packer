96cd3cf1b1ef84f34811c05391da6e7138fdb638
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-04-02 10:49:09 -0600

    setup test

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,24 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
-    client: 'pg',
-    connection: 'postgres://localhost/mars_packer',
-    migrations: {
-      directory: './db/migrations'
+      client: 'pg',
+      connection: 'postgres://localhost/mars_packer',
+      migrations: {
+        directory: './db/migrations'
+      },
+      seeds: {
+        directory: './db/seeds/dev'
+      },
+      useNullAsDefault: true
     },
-    seeds: {
-      directory: './db/seeds/dev'
-    },
-    useNullAsDefault: true
-  }
+    test: {
+        client: 'pg',
+        connection: 'postgres://localhost/mars_packer_test',
+        migrations: {
+          directory: './db/migrations'
+        },
+        seeds: {
+          directory: './db/seeds/test'
+        },
+        useNullAsDefault: true
+      }
 };
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.use(express.static('public'));
 
 app.listen(app.get('port'), () => {
